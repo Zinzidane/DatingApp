20b1c83cf1759bf15956659ac0bbbfb4ec352609
Ivan Muliarchyk <ya-gans@mail.ru>
2020-05-07 20:02:45 +0300

    Add auth in client app

---
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
+})
+export class RegisterComponent implements OnInit {
+  model: any = {};
+  @Output() cancelRegister = new EventEmitter();
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {}
+
+  register() {
+    this.authService.register(this.model).subscribe();
+  }
+
+  cancel() {
+    this.cancelRegister.emit();
+  }
+}
