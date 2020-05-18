b4ce18617ac5b339a33163d4255418cffe65fe06
Ivan Muliarchyk <ya-gans@mail.ru>
2020-05-18 23:40:27 +0300

    Configure data context for identity

--- DatingApp.API/Models/Role.cs
+++ DatingApp.API/Models/Role.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace DatingApp.API.Models
 {
-    public class Role : IndentityRole<int>
+    public class Role : IdentityRole<int>
     {
         public virtual ICollection<UserRole> UserRoles { get; set; }
     }
